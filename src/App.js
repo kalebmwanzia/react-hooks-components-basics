f413fa4b9f3579c12aaee72ec2c83738dcfc15d4
caleb <mwanziacaleb90@gmail.com>
2023-10-25 23:26:14 +0300

    completed assignment

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import Article from "./Article";
 import Comment from "./Comment";
 
+
 function App() {
   return (
     <div>
-      <Article />
-      <Comment />
-    </div>
+    <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div>
+    <div>Naturally, I agree with this article.</div>
+  </div>
   );
 }
 
